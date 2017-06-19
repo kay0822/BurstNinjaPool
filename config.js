5de473cd26d53d7140bc84008a1bbdbb0d7c56c1
Samuel Chrisitson <samuel-christison@hotmail.com>
2017-06-20 01:18:58 +1200

    Inital Commit
    BURST-ZNEH-ZB8X-9T38-HSND9

---
+++ config.js
@@ -0,0 +1,49 @@
+{
+	"bindAddress": "0.0.0.0",
+	"listenPort": "8125",
+	"documentRoot": "./static",
+	"databasePoolSize": "3",
+	"databaseURI": "unix:///var/lib/mysql/mysql.sock",
+	"databaseSchema": "burstpool",
+	"databaseUser": "ninja",
+	"databasePassword": "iaushd978h9fd2fh",
+	
+	"pushURL": "https://api.pushover.net/1/messages.json",
+	"pushKey": "aWQfhxr3crixBnq",
+	"pushRecipient": "gBa9n",
+
+	"burstServers": [ "util.burst-team.us:8889", "wallet.burstcoin.tech:8750" ],
+	"burstServerTimeout": 2,
+
+	"poolName": "BURST Ninja!",
+	"poolHostname": "burst.ninja",
+
+	"accountUpdateTimeout": 600,
+	"poolNumericAccountId": "12536",
+	"poolEncodedPassphrase": "xxxxxxxxxxxxx",
+
+	"poolDeadlineLimit": 386400,
+	"poolDeadlineReallyBad": "3153600000",
+
+	"historicBlockCount": 500,
+	"capacityBlockCount": 500,
+
+	"submitNonceCooloff": 0,
+
+	"poolFeePercent": 2,
+	"poolFeeNumericAccountId": "1542",
+
+	"minimumPayout": 100,
+	
+	"sharePowerFactor": 1.2,
+	"currentBlockRewardPercent": 60,
+
+	"recentBlockHistoryDepth": 25,
+
+	"maximumPayoutBlockDelay": 20,
+	"minimumPayoutBlockDelay": 4,
+
+	"bonusNumericAccountId": "",
+	"bonusEncodedPassphrase": "xxxxxxxxxxxxxxxxxxxx",
+	"bonusMinimumThreshold": 20
+}
